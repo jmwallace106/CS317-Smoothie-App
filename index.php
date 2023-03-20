840b6fbf0b9f69bbed4de43ab97ac6e5a00c1bb3
nfb20137 <alistair.dow.2020@uni.strath.ac.uk>
2023-03-20 18:18:51 +0000

    Base page login and register option Start for register page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <title>SmoothI</title>
+    <link rel="stylesheet" href="smoothI.css">
+</head>
+
+<body>
+    <header><h1>SmoothI</h1></header>
+
+    <div id = "smoothIGraphics">
+        <img src = "smoothieIcon.png" alt = "smoothieIcon" width="90%">
+    </div>
+
+    <div id = "bottomPart">
+        <div id = "welcomeInfo">
+            <h2>Introducing SmoothI</h2>
+            <p>Find new smoothie recipes<br>See others creations<br>Post your own!</p>
+        </div>
+
+        <b id = "loginSection">
+            <br><button onclick="window.location.href = 'loginmakelater.php'">Login</button>
+        </div>
+
+        <div id = "registerSection">
+            <br><button onclick="window.location.href = 'register.php'">Register</button>
+        </div>
+    </div>
+
+</body>
+
